ca8bb7c09b4a2d9b6bc9e527aa4cd9db12f136d0
Sophia <sjc01@mail.com>
2013-10-18 11:16:59 +1100

    Create index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+  require_once("MiniTemplator.class.php");
+  
+  $t = new MiniTemplator;
+  
+  $->readTemplateFromfile("template.html");
+  
+  $content = "";
+  
+  $t->setVariable ("content", $content);
+  $t->addBlock("content_block");
+  
+  $t->generateOutput();
+?>
