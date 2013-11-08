caaee464ae6d3e94c07323c155be2aa81e0ae76e
MRJS311 <jaren@windenrain.com.au>
2013-11-08 11:05:49 +1100

    Website Update
    Template update, index is now login page, home is main page.

--- ChisholmApp/Pages/index.php
+++ ChisholmApp/Pages/index.php
@@ -1,33 +1,41 @@
 <%@LANGUAGE="JAVASCRIPT" CODEPAGE="65001"%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<html xmlns="http://www.w3.org/1999/xhtml"><!-- InstanceBegin template="/Templates/template.dwt.asp" codeOutsideHTMLIsLocked="false" -->
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<!-- TemplateBeginEditable name="doctitle" -->
+<!-- InstanceBeginEditable name="doctitle" -->
 <title>Untitled Document</title>
-<!-- TemplateEndEditable -->
-<!-- TemplateBeginEditable name="head" -->
-<!-- TemplateEndEditable -->
-<link href="../../../../Dip. Website Dev/Website/ChisholmApp.css" rel="stylesheet" type="text/css" />
+<!-- InstanceEndEditable -->
+<!-- InstanceBeginEditable name="head" -->
+<!-- InstanceEndEditable -->
+<link href="file:///C|/Users/Jaren/Documents/Dip.%20Website%20Dev/Website/ChisholmApp.css" rel="stylesheet" type="text/css" />
 </head>
 
 <body>
 <div class="wrapper">
   <div class="header">
-	<img src="../../../../Dip. Website Dev/Website/chisholm-institute.jpg" alt="Chisholm Logo" width="100%" height="100%" />
+	<img src="file:///C|/Users/Jaren/Documents/Dip.%20Website%20Dev/Website/chisholm-institute.jpg" alt="Chisholm Logo" width="100%" height="100%" />
   </div>
-  <div class="content" align="center"><!-- TemplateBeginEditable name="contentRegion" -->
-    <p>Welcome</p>
-    <p>To find your next class please choose from one of the following options.</p>
-    <p><a href="" class="link">Date</a></p>
-    <p class="link"><a href="" class="link">Time</a></p>
-    <p class="link"><a href="" class="link">Subject</a></p>
-    <p class="link"><a href="" class="link">Campus</a></p>
-    <p class="link"><a href="" class="link">Room</a></p>
-  <!-- TemplateEndEditable --></div>
+  <div class="content" align="center"><!-- InstanceBeginEditable name="contentRegion" -->
+    <p align="center">    
+    <form id="form1" name="form1" method="post" action="">
+      <p>
+        <label for="textfield">Username:</label>
+        <input type="text" name="textfield" id="textfield" />
+      </p>
+      <p>
+        <label for="password">Password:</label>
+        <input type="password" name="password" id="password" />
+      </p>
+      <p>
+        <input type="submit" name="submit" id="submit" value="Submit" />
+      </p>
+    </form>
+    </p>
+  <!-- InstanceEndEditable --></div>
   <div class="footer" align="center">
     <p><a href="index.php" class="link">Home</a></p>
   </div>
 </div>
 </body>
-</html>
+<!-- InstanceEnd --></html>
