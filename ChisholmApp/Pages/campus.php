559f8c11be967e85a5de2a31ebb147bf36c3dca6
Raiser00XN <raiser00xn@gmail.com>
2013-11-14 17:28:17 +1100

    Create campus.php

---
+++ ChisholmApp/Pages/campus.php
@@ -0,0 +1,51 @@
+<%@LANGUAGE="JAVASCRIPT" CODEPAGE="65001"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<!-- TemplateBeginEditable name="doctitle" -->
+<title>Campus</title>
+<!-- TemplateEndEditable -->
+<!-- TemplateBeginEditable name="head" -->
+<!-- TemplateEndEditable -->
+<link href="../../../../Dip. Website Dev/Website/ChisholmApp.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+<div class="wrapper">
+  <div class="header">
+        <img src="../../../../Dip. Website Dev/Website/chisholm-institute.jpg" alt="Chisholm Logo" width="100%" height="100%" />
+  </div>
+  <div class="content">
+  <!-- TemplateBeginEditable name="contentRegion" -->
+    <p align="center">Campus</p>
+        <table width="200" border="0" align="center">
+                  <tr>
+                            <td align="right">Campus</td>
+                            <td align="left">&nbsp;</td>
+                  </tr>
+                  <tr>
+                            <td align="right">Date</td>
+                            <td align="left">&nbsp;</td>
+                  </tr>
+                  <tr>
+                            <td align="right">Time</td>
+                            <td align="left">&nbsp;</td>
+                  </tr>
+                  <tr>
+                            <td align="right">Room</td>
+                            <td align="left">&nbsp;</td>
+                  </tr>
+                  <tr>
+                            <td align="right">Subject</td>
+                            <td align="left">&nbsp;</td>
+                  </tr>
+        </table>
+  <!-- TemplateEndEditable -->
+  </div>
+  <div class="footer" align="center">
+    <p><a href="index.php" class="link">Home</a></p>
+  </div>
+</div>
+</body>
+</html>
