b1c15b757fec110b521950297124b2ca6cabf1a3
mitchelldeka <mitchell.deka@gmail.com>
2013-11-15 11:23:21 +1100

    Create day.php

---
+++ ChisholmApp/Pages/day.php
@@ -0,0 +1,51 @@
+<%@LANGUAGE="JAVASCRIPT" CODEPAGE="65001"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<!-- TemplateBeginEditable name="doctitle" -->
+<title>Day</title>
+<!-- TemplateEndEditable -->
+<!-- TemplateBeginEditable name="head" -->
+<!-- TemplateEndEditable -->
+<link href="../../../../Dip. Website Dev/Website/ChisholmApp.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+<div class="wrapper">
+  <div class="header">
+        <img src="../../../../Dip. Website Dev/Website/chisholm-institute.jpg" alt="Chisholm Logo" width="100%" height="100%" />
+  </div>
+  <div class="content">
+  <!-- TemplateBeginEditable name="contentRegion" -->
+    <p align="center">Day</p>
+    <table width="200" border="0">
+      <tr>
+        <td width="94">Day:</td>
+        <td width="96">&nbsp;</td>
+      </tr>
+      <tr>
+        <td>Time:</td>
+        <td>&nbsp;</td>
+      </tr>
+      <tr>
+        <td>Room:</td>
+        <td>&nbsp;</td>
+      </tr>
+      <tr>
+        <td>Campus:</td>
+        <td>&nbsp;</td>
+      </tr>
+      <tr>
+        <td>Subject:</td>
+        <td>&nbsp;</td>
+      </tr>
+    </table>
+  <!-- TemplateEndEditable -->
+  </div>
+  <div class="footer" align="center">
+    <p><a href="index.php" class="link">Home</a></p>
+  </div>
+</div>
+</body>
+</html>
