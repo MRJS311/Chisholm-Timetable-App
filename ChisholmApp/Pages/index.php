abf62eec4caf7f410d125c93a9965f94880be9d9
MRJS311 <jaren@windenrain.com.au>
2013-11-24 12:18:54 +1100

    Additional Changes
    Response now Working.

--- ChisholmApp/Pages/index.php
+++ ChisholmApp/Pages/index.php
@@ -2,6 +2,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml"><!-- InstanceBegin template="/Templates/template.dwt.asp" codeOutsideHTMLIsLocked="false" -->
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <!-- InstanceBeginEditable name="doctitle" -->
 <title>Welcome</title>
 <!-- InstanceEndEditable -->
