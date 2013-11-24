7755dfb932b6499dc4e725d245e10867d3acea3c
sjc01 <sjc01@mail.com>
2013-11-24 14:37:41 +1100

    changed spelling from firday to Friday

--- ChisholmApp/Pages/day.php
+++ ChisholmApp/Pages/day.php
@@ -29,7 +29,7 @@
     			  <option value="Tuesday">Tuesday</option>
     	  		<option value="Wednesday">Wednesday</option>
     	  		<option value="Thursday">Thursday</option>
-    	  		<option value="Friday">Firday</option>
+    	  		<option value="Friday">Friday</option>
     	  		<option value="Saturday">Saturday</option>
 	  	    </select>
 	      </td>
